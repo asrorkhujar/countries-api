1ac322ffee2b1a1b22a96b45d504dd79730e5f72
coder-1990 <asrorakmalovich@gmail.com>
2021-10-05 14:02:27 +0500

    reindent

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // GLOBAL VARS
-let COUNTRIES_BASE_URL = 'https://restcountries.com/v3.1/name/';
+let COUNTRIES_BASE_URL = 'https://restcountries.com/v3.1';
 
 // DOM ELEMENTS
 const elCountriesForm = document.querySelector('.countries__form');
@@ -13,34 +13,33 @@ const elCountryModal = document.querySelector('#country-modal');
 const elCountryModalName = elCountryModal.querySelector('.info-country__name');
 
 // FUNCTIONS
-function getJSON(url, successFn, errorFn){
+function getJSON(url, successFn, errorFn) {
   fetch(url)
-  .then(response => response.json())
-  .then((data) =>  {
+    .then(response => response.json())
+    .then((data) => {
 
-    if (data.status !== 404 ) {
-      successFn(data);
-    }else{
-      errorFn();
-    }
-  })
+      if (data.status !== 404) {
+        successFn(data);
+      } else {
+        errorFn();
+      }
+    })
 }
 
-function showCountries(countries){
+function showCountries(countries) {
   elCountriesList.innerHTML = '';
   const elCountriesFragment = document.createDocumentFragment();
-  const elCountryItem = elCountryTemplateItem.cloneNode(true);
 
   countries.forEach(country => {
-
-    elCountryItem.querySelector('img').src = country.flags.svg;
+    let elCountryItem = elCountryTemplateItem.cloneNode(true);
+    elCountryItem.querySelector('img').src = country.flags.png;
     elCountryItem.querySelector('.country__name').textContent = country.name.common;
     elCountryItem.querySelector('.country__population').textContent = country.population;
     elCountryItem.querySelector('.country__region').textContent = country.region;
-    elCountryItem.querySelector('.country__capital').textContent = country.capital[0];
+    elCountryItem.querySelector('.country__capital').textContent = country.capital;
     elCountryItem.querySelector('.country__more-btn').dataset.name = country.name.common;
-    elCountriesFragment.appendChild(elCountryItem);
 
+    elCountriesFragment.appendChild(elCountryItem);
   });
 
   elCountriesList.appendChild(elCountriesFragment);
@@ -55,15 +54,24 @@ function showCountriesError() {
 function onCounryFormSubmit(evt) {
   evt.preventDefault();
 
-  let searchUrl = `${COUNTRIES_BASE_URL}${elCountrySearchInput.value.trim()}`;
+  let searchUrl = `${COUNTRIES_BASE_URL}/name/${elCountrySearchInput.value.trim()}`;
   getJSON(searchUrl, showCountries, showCountriesError);
 }
 
-function onClickCountryList(evt){
+function onClickCountryList(evt) {
   if (evt.target.matches('.country__more-btn')) {
-    let modalUrl = `${COUNTRIES_BASE_URL}${evt.target.dataset.name}`;
-    getJSON(modalUrl, updateCountryModal, showCountriesError );
+    let modalUrl = `${COUNTRIES_BASE_URL}/name/${evt.target.dataset.name}`;
+    getJSON(modalUrl, updateCountryModal, showCountriesError);
   }
+}
+
+function clearModal() {
+  elCountryItem.querySelector('img').src = '';
+  elCountryItem.querySelector('.country__name').textContent = '';
+  elCountryItem.querySelector('.country__population').textContent ='' ;
+  elCountryItem.querySelector('.country__region').textContent = '';
+  elCountryItem.querySelector('.country__capital').textContent = '';
+  elCountryItem.querySelector('.country__more-btn').dataset.name = '';
 }
 
 function updateCountryModal(data) {
@@ -71,14 +79,20 @@ function updateCountryModal(data) {
   elCountryModal.querySelector('.info-country__img').src = data[0].flags.svg;
   elCountryModal.querySelector('.country__details-region').textContent = data[0].region;
 
-  elCountryModal.querySelector('.country__details-currency').textContent = data[0].currencies.name;
+  elCountryModal.querySelector('.country__details-currency').textContent = Object.keys(data[0].currencies).join(', ');
   // elCountryModal.querySelector('.country__details-currency').textContent = data[0].languages.join(', ');
   elCountryModal.querySelector('.country__details-borders').textContent = data[0].borders.join(', ');
 }
 
+getJSON(`${COUNTRIES_BASE_URL}/all`, showCountries, showCountriesError);
+
 // EVENT LISTENERS
 if (elCountriesForm) {
   elCountriesForm.addEventListener('submit', onCounryFormSubmit);
+}
+
+if (elCountryModal) {
+  elCountryModal.addEventListener('hide.bs.modal', clearModal)
 }
 
 if (elCountriesList) {
