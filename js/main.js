80abc510297d58ea19630fee0611bd7cfdaed880
coder-1990 <asrorakmalovich@gmail.com>
2021-10-05 16:06:21 +0500

    add google maps

--- js/main.js
+++ js/main.js
@@ -76,13 +76,13 @@ function updateCountryModal(data) {
   elCountryModalName.textContent = data[0].name.common;
   elCountryModal.querySelector('.info-country__img').src = data[0].flags.svg;
   elCountryModal.querySelector('.country__details-region').textContent = data[0].region;
-
+  elCountryModal.querySelector('.country__details-subregion').textContent = data[0].subregion;
+  elCountryModal.querySelector('.country__details-maps').href = data[0].maps.googleMaps;
   // elCountryModal.querySelector('.country__details-currency').textContent = Object.keys(data[0].currencies).join(', ');
 
   //Asrorxo'ja aka bu variant ham bor aka "Abdulaziz" //
   elCountryModal.querySelector('.country__details-currency').textContent = Object.values(data[0].currencies)[0].name;
-
-  // elCountryModal.querySelector('.country__details-currency').textContent = data[0].languages.join(', ');
+  elCountryModal.querySelector('.country__details-language').textContent = Object.keys(data[0].languages).join(', ');
   elCountryModal.querySelector('.country__details-borders').textContent = data[0].borders.join(', ');
 }
 
